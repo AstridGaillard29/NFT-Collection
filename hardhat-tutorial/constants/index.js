95522f5deef24fdf8c752ad46d46725493d8d08a
Astrid Gaillard <astridgaillard@macbook-pro-de-astrid.home>
2022-06-14 20:18:17 +0200

    first commits

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -3,4 +3,4 @@ const WHITELIST_CONTRACT_ADDRESS = "0x0693A427d12d1C4Ba7a5C7965b78D8C1bA3e1f8f";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }; 
